51f734eafff4b0b49480d9cd6085f5c624676d50
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2021-03-07 19:32:21 +0400

    Pilot

---
+++ HaleyEvents/Interfaces/ISubscriber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haley.Abstractions
+{
+    public interface ISubscriber
+    {
+        string id { get; set; }
+        Type declaring_type { get;}
+        string listener_method { get; set; }
+        void sendMessage(params object[] args);
+    }
+}
